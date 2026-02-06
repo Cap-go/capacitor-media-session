ad124e6fbb118341578aca98bfbd9363d8fcff73
Martin Donadieu <martindonadieu@gmail.com>
2026-02-06 03:20:26 +0000

    fix: wrong bump

--- example-app/ios/App/CapApp-SPM/Package.swift
+++ example-app/ios/App/CapApp-SPM/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.2"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
         .package(name: "CapgoCapacitorMediaSession", path: "../../../node_modules/.bun/@capgo+capacitor-media-session@file+../node_modules/@capgo/capacitor-media-session")
     ],
     targets: [
